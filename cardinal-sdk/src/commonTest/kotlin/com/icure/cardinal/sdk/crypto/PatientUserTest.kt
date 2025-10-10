f9e59ec2108bdf82c76f9477d4a8e5d54eb343ad
Luca Tremamunno <tremaluca96@gmail.com>
2025-10-10 16:50:16 +0200

    Better error reporting for delegating to user with no keys when user is a patient

--- cardinal-sdk/src/commonTest/kotlin/com/icure/cardinal/sdk/crypto/PatientUserTest.kt
+++ cardinal-sdk/src/commonTest/kotlin/com/icure/cardinal/sdk/crypto/PatientUserTest.kt
@@ -1,17 +1,24 @@
 package com.icure.cardinal.sdk.crypto
 
+import com.icure.cardinal.sdk.api.raw.impl.RawHealthcarePartyApiImpl
 import com.icure.cardinal.sdk.filters.HealthElementFilters
 import com.icure.cardinal.sdk.model.DataOwnerWithType
 import com.icure.cardinal.sdk.model.DecryptedHealthElement
 import com.icure.cardinal.sdk.model.DecryptedPatient
 import com.icure.cardinal.sdk.model.EncryptedPatient
+import com.icure.cardinal.sdk.model.HealthcareParty
 import com.icure.cardinal.sdk.model.embed.AccessLevel
+import com.icure.cardinal.sdk.test.DefaultRawApiConfig
 import com.icure.cardinal.sdk.test.autoCancelJob
+import com.icure.cardinal.sdk.test.baseUrl
 import com.icure.cardinal.sdk.test.createHcpUser
 import com.icure.cardinal.sdk.test.createPatientUser
 import com.icure.cardinal.sdk.test.createUserFromExistingPatient
 import com.icure.cardinal.sdk.test.initializeTestEnvironment
+import com.icure.cardinal.sdk.test.superadminAuth
+import com.icure.cardinal.sdk.test.testGroupId
 import com.icure.kryptom.crypto.defaultCryptoService
+import com.icure.utils.InternalIcureApi
 import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.StringSpec
 import io.kotest.matchers.collections.shouldContainAll
@@ -20,8 +27,10 @@ import io.kotest.matchers.collections.shouldNotContainAnyOf
 import io.kotest.matchers.nulls.shouldNotBeNull
 import io.kotest.matchers.shouldBe
 import io.kotest.matchers.shouldNotBe
+import io.kotest.matchers.string.shouldContain
 import io.kotest.matchers.types.shouldBeInstanceOf
 
+@OptIn(InternalIcureApi::class)
 class PatientUserTest : StringSpec({
 	val specJob = autoCancelJob()
 	beforeSpec {
@@ -110,5 +119,62 @@ class PatientUserTest : StringSpec({
 		patientApi.healthElement.filterHealthElementsBy(
 			HealthElementFilters.byPatientsForSelf(listOf(initializedPatient))
 		).next(5).shouldHaveSize(1).first().note.shouldNotBeNull() shouldBe createdData.note
+	}
+
+	"A new patient user should be able to initialize his encryption metadata and share with another user" {
+		val patientDetails = createPatientUser()
+		val hcpDetails = createHcpUser()
+		val hcpApi = hcpDetails.api(specJob)
+		val patientApi = patientDetails.api(specJob)
+		patientApi.patient.tryAndRecover.getPatient(patientDetails.dataOwnerId).shouldBeInstanceOf<EncryptedPatient>()
+		val initializedEncryptedPatient = patientApi.patient.ensureEncryptionMetadataForSelfIsInitialized(mapOf(hcpDetails.dataOwnerId to AccessLevel.Write))
+		patientApi.patient.encrypted.getPatient(patientDetails.dataOwnerId) shouldBe initializedEncryptedPatient
+		val initializedPatient = patientApi.patient.decrypt(listOf(initializedEncryptedPatient)).single()
+		val updatedPatient = patientApi.patient.modifyPatient(initializedPatient.copy(note = "My secret note"))
+		updatedPatient.rev shouldNotBe initializedPatient.rev
+		patientApi.patient.getPatient(updatedPatient.id).shouldNotBeNull().note shouldBe "My secret note"
+		hcpApi.patient.getPatient(updatedPatient.id) shouldBe updatedPatient
+		val createdData = patientApi.healthElement.createHealthElement(
+			patientApi.healthElement.withEncryptionMetadata(
+				DecryptedHealthElement(
+					id = defaultCryptoService.strongRandom.randomUUID(),
+					note = "Some note"
+				),
+				initializedPatient,
+				patientApi.user.getCurrentUser(),
+				mapOf(hcpDetails.dataOwnerId to AccessLevel.Write)
+			)
+		).shouldNotBeNull()
+		// The patient should be able to find back his data
+		patientApi.healthElement.filterHealthElementsBy(
+			HealthElementFilters.byPatientsForSelf(listOf(initializedPatient))
+		).next(5).shouldHaveSize(1).first().note.shouldNotBeNull() shouldBe createdData.note
+		hcpApi.healthElement.filterHealthElementsBy(
+			HealthElementFilters.byPatientsForSelf(listOf(initializedPatient))
+		).next(5).shouldHaveSize(1).first().note.shouldNotBeNull() shouldBe createdData.note
+	}
+
+	"Initializing the encryption metadata for a new patient user should fail with appropriate exception if attempting to share with hcp that has no key" {
+		val hcpRawApi = RawHealthcarePartyApiImpl(baseUrl, superadminAuth, DefaultRawApiConfig)
+		val hcpId = defaultCryptoService.strongRandom.randomUUID()
+		hcpRawApi.createHealthcarePartyInGroup(
+			testGroupId,
+			HealthcareParty(
+				hcpId,
+				firstName = "Hcp-$hcpId",
+				lastName = "Hcp-$hcpId",
+			)
+		).successBody()
+		val patientDetails = createPatientUser()
+		val patientApi = patientDetails.api(specJob)
+		shouldThrow<IllegalArgumentException> {
+			patientApi.patient.ensureEncryptionMetadataForSelfIsInitialized(
+				mapOf(hcpId to AccessLevel.Write)
+			)
+		}.message.shouldNotBeNull().lowercase().also {
+			it shouldContain "delegate"
+			it shouldContain "keys"
+			it shouldContain hcpId
+		}
 	}
 })
