44021c2e9e3affdc5feb52ec569dd7786124c4fd
Vincenzo Pierro <vcp@icure.com>
2025-10-13 15:33:35 +0200

    Temporary fix on application settings

--- cardinal-sdk/src/commonMain/kotlin/com/icure/cardinal/sdk/api/impl/ApplicationSettingsApiImpl.kt
+++ cardinal-sdk/src/commonMain/kotlin/com/icure/cardinal/sdk/api/impl/ApplicationSettingsApiImpl.kt
@@ -3,6 +3,7 @@ package com.icure.cardinal.sdk.api.impl
 import com.icure.cardinal.sdk.api.ApplicationSettingsApi
 import com.icure.cardinal.sdk.api.raw.RawApplicationSettingsApi
 import com.icure.cardinal.sdk.model.ApplicationSettings
+import com.icure.cardinal.sdk.model.EncryptedApplicationSettings
 import com.icure.utils.InternalIcureApi
 
 @InternalIcureApi
@@ -11,9 +12,9 @@ internal class ApplicationSettingsApiImpl(
 ) : ApplicationSettingsApi {
 	override suspend fun getApplicationSettings() = rawApi.getApplicationSettings().successBody()
 
-	override suspend fun createApplicationSettings(applicationSettings: ApplicationSettings) =
+	override suspend fun createApplicationSettings(applicationSettings: EncryptedApplicationSettings) =
 		rawApi.createApplicationSettings(applicationSettings).successBody()
 
-	override suspend fun updateApplicationSettings(applicationSettings: ApplicationSettings) =
+	override suspend fun updateApplicationSettings(applicationSettings: EncryptedApplicationSettings) =
 		rawApi.updateApplicationSettings(applicationSettings).successBody()
 }
