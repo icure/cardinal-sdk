56f2f2f4f4f655828ac2115b87c94642596a3fd2
Luca Tremamunno <tremaluca96@gmail.com>
2026-04-10 16:49:08 +0200

    Fix resolution of newly added filter serializers, add more missing filter serializers

--- cardinal-sdk/src/commonMain/kotlin/com/icure/cardinal/sdk/utils/Serialization.kt
+++ cardinal-sdk/src/commonMain/kotlin/com/icure/cardinal/sdk/utils/Serialization.kt
@@ -16,20 +16,32 @@ import com.icure.cardinal.sdk.model.embed.form.template.TimePicker
 import com.icure.cardinal.sdk.model.filter.AbstractFilter
 import com.icure.cardinal.sdk.model.filter.chain.FilterChain
 import com.icure.cardinal.sdk.model.notification.Subscription
+import com.icure.cardinal.sdk.serialization.AccessLogAbstractFilterSerializer
+import com.icure.cardinal.sdk.serialization.AgendaAbstractFilterSerializer
 import com.icure.cardinal.sdk.serialization.AnyAbstractFilterSerializer
+import com.icure.cardinal.sdk.serialization.CalendarItemAbstractFilterSerializer
+import com.icure.cardinal.sdk.serialization.ClassificationAbstractFilterSerializer
 import com.icure.cardinal.sdk.serialization.CodeAbstractFilterSerializer
 import com.icure.cardinal.sdk.serialization.ContactAbstractFilterSerializer
 import com.icure.cardinal.sdk.serialization.DeviceAbstractFilterSerializer
+import com.icure.cardinal.sdk.serialization.DocumentAbstractFilterSerializer
 import com.icure.cardinal.sdk.serialization.EncryptableSerializationModule
 import com.icure.cardinal.sdk.serialization.FilterChainSerializer
+import com.icure.cardinal.sdk.serialization.FormAbstractFilterSerializer
+import com.icure.cardinal.sdk.serialization.FormTemplateAbstractFilterSerializer
+import com.icure.cardinal.sdk.serialization.GroupAbstractFilterSerializer
 import com.icure.cardinal.sdk.serialization.HealthElementAbstractFilterSerializer
 import com.icure.cardinal.sdk.serialization.HealthcarePartyAbstractFilterSerializer
+import com.icure.cardinal.sdk.serialization.InsuranceAbstractFilterSerializer
 import com.icure.cardinal.sdk.serialization.InvoiceAbstractFilterSerializer
 import com.icure.cardinal.sdk.serialization.MaintenanceTaskAbstractFilterSerializer
+import com.icure.cardinal.sdk.serialization.MedicalLocationAbstractFilterSerializer
 import com.icure.cardinal.sdk.serialization.MessageAbstractFilterSerializer
 import com.icure.cardinal.sdk.serialization.PatientAbstractFilterSerializer
 import com.icure.cardinal.sdk.serialization.ServiceAbstractFilterSerializer
 import com.icure.cardinal.sdk.serialization.SubscriptionSerializer
+import com.icure.cardinal.sdk.serialization.TarificationAbstractFilterSerializer
+import com.icure.cardinal.sdk.serialization.TimeTableAbstractFilterSerializer
 import com.icure.cardinal.sdk.serialization.TopicAbstractFilterSerializer
 import com.icure.cardinal.sdk.serialization.UserAbstractFilterSerializer
 import com.icure.utils.InternalIcureApi
@@ -95,20 +107,30 @@ object Serialization {
 	private fun <T : Identifiable<String>> getContextualAbstractFilter(genericSerializers: List<KSerializer<*>>): KSerializer<AbstractFilter<T>> =
 		when (val serialName = genericSerializers.single().descriptor.serialName) {
 			"AbstractFilter<Identifiable>", "kotlinx.serialization.Polymorphic<Identifiable>" -> AnyAbstractFilterSerializer as KSerializer<AbstractFilter<T>>
-			"kotlinx.serialization.Polymorphic<MaintenanceTask>", "AbstractFilter<MaintenanceTask>", "com.icure.cardinal.sdk.model.MaintenanceTask" -> MaintenanceTaskAbstractFilterSerializer as KSerializer<AbstractFilter<T>>
+			"kotlinx.serialization.Polymorphic<AccessLog>", "AbstractFilter<AccessLog>", "com.icure.cardinal.sdk.model.AccessLog" -> AccessLogAbstractFilterSerializer as KSerializer<AbstractFilter<T>>
+			"kotlinx.serialization.Polymorphic<Agenda>", "AbstractFilter<Agenda>", "com.icure.cardinal.sdk.model.Agenda" -> AgendaAbstractFilterSerializer as KSerializer<AbstractFilter<T>>
+			"kotlinx.serialization.Polymorphic<CalendarItem>", "AbstractFilter<CalendarItem>", "com.icure.cardinal.sdk.model.CalendarItem" -> CalendarItemAbstractFilterSerializer as KSerializer<AbstractFilter<T>>
+			"kotlinx.serialization.Polymorphic<Classification>", "AbstractFilter<Classification>", "com.icure.cardinal.sdk.model.Classification" -> ClassificationAbstractFilterSerializer as KSerializer<AbstractFilter<T>>
 			"kotlinx.serialization.Polymorphic<Code>", "AbstractFilter<Code>", "com.icure.cardinal.sdk.model.Code" -> CodeAbstractFilterSerializer as KSerializer<AbstractFilter<T>>
 			"kotlinx.serialization.Polymorphic<Contact>", "AbstractFilter<Contact>", "com.icure.cardinal.sdk.model.Contact" -> ContactAbstractFilterSerializer as KSerializer<AbstractFilter<T>>
 			"kotlinx.serialization.Polymorphic<Device>", "AbstractFilter<Device>", "com.icure.cardinal.sdk.model.Device" -> DeviceAbstractFilterSerializer as KSerializer<AbstractFilter<T>>
-			"kotlinx.serialization.Polymorphic<HealthcareParty>", "AbstractFilter<HealthcareParty>", "com.icure.cardinal.sdk.model.HealthcareParty" -> HealthcarePartyAbstractFilterSerializer as KSerializer<AbstractFilter<T>>
+			"kotlinx.serialization.Polymorphic<Document>", "AbstractFilter<Document>", "com.icure.cardinal.sdk.model.Document" -> DocumentAbstractFilterSerializer as KSerializer<AbstractFilter<T>>
+			"kotlinx.serialization.Polymorphic<Form>", "AbstractFilter<Form>", "com.icure.cardinal.sdk.model.Form" -> FormAbstractFilterSerializer as KSerializer<AbstractFilter<T>>
+			"kotlinx.serialization.Polymorphic<FormTemplate>", "AbstractFilter<FormTemplate>", "com.icure.cardinal.sdk.model.FormTemplate" -> FormTemplateAbstractFilterSerializer as KSerializer<AbstractFilter<T>>
+			"kotlinx.serialization.Polymorphic<Group>", "AbstractFilter<Group>", "com.icure.cardinal.sdk.model.Group" -> GroupAbstractFilterSerializer as KSerializer<AbstractFilter<T>>
 			"kotlinx.serialization.Polymorphic<HealthElement>", "AbstractFilter<HealthElement>", "com.icure.cardinal.sdk.model.HealthElement" -> HealthElementAbstractFilterSerializer as KSerializer<AbstractFilter<T>>
+			"kotlinx.serialization.Polymorphic<HealthcareParty>", "AbstractFilter<HealthcareParty>", "com.icure.cardinal.sdk.model.HealthcareParty" -> HealthcarePartyAbstractFilterSerializer as KSerializer<AbstractFilter<T>>
+			"kotlinx.serialization.Polymorphic<Insurance>", "AbstractFilter<Insurance>", "com.icure.cardinal.sdk.model.Insurance" -> InsuranceAbstractFilterSerializer as KSerializer<AbstractFilter<T>>
 			"kotlinx.serialization.Polymorphic<Invoice>", "AbstractFilter<Invoice>", "com.icure.cardinal.sdk.model.Invoice" -> InvoiceAbstractFilterSerializer as KSerializer<AbstractFilter<T>>
+			"kotlinx.serialization.Polymorphic<MaintenanceTask>", "AbstractFilter<MaintenanceTask>", "com.icure.cardinal.sdk.model.MaintenanceTask" -> MaintenanceTaskAbstractFilterSerializer as KSerializer<AbstractFilter<T>>
+			"kotlinx.serialization.Polymorphic<MedicalLocation>", "AbstractFilter<MedicalLocation>", "com.icure.cardinal.sdk.model.MedicalLocation" -> MedicalLocationAbstractFilterSerializer as KSerializer<AbstractFilter<T>>
 			"kotlinx.serialization.Polymorphic<Message>", "AbstractFilter<Message>", "com.icure.cardinal.sdk.model.Message" -> MessageAbstractFilterSerializer as KSerializer<AbstractFilter<T>>
 			"kotlinx.serialization.Polymorphic<Patient>", "AbstractFilter<Patient>", "com.icure.cardinal.sdk.model.Patient" -> PatientAbstractFilterSerializer as KSerializer<AbstractFilter<T>>
 			"kotlinx.serialization.Polymorphic<Service>", "AbstractFilter<Service>", "com.icure.cardinal.sdk.model.embed.Service" -> ServiceAbstractFilterSerializer as KSerializer<AbstractFilter<T>>
+			"kotlinx.serialization.Polymorphic<Tarification>", "AbstractFilter<Tarification>", "com.icure.cardinal.sdk.model.Tarification" -> TarificationAbstractFilterSerializer as KSerializer<AbstractFilter<T>>
+			"kotlinx.serialization.Polymorphic<TimeTable>", "AbstractFilter<TimeTable>", "com.icure.cardinal.sdk.model.TimeTable" -> TimeTableAbstractFilterSerializer as KSerializer<AbstractFilter<T>>
 			"kotlinx.serialization.Polymorphic<Topic>", "AbstractFilter<Topic>", "com.icure.cardinal.sdk.model.Topic" -> TopicAbstractFilterSerializer as KSerializer<AbstractFilter<T>>
 			"kotlinx.serialization.Polymorphic<User>", "AbstractFilter<User>", "com.icure.cardinal.sdk.model.User" -> UserAbstractFilterSerializer as KSerializer<AbstractFilter<T>>
-			"kotlinx.serialization.Polymorphic<CalendarItem>", "AbstractFilter<CalendarItem>", "com.icure.cardinal.sdk.model.CalendarItem" -> UserAbstractFilterSerializer as KSerializer<AbstractFilter<T>>
-			"kotlinx.serialization.Polymorphic<Form>", "AbstractFilter<Form>", "com.icure.cardinal.sdk.model.Form" -> UserAbstractFilterSerializer as KSerializer<AbstractFilter<T>>
 			else -> throw SerializationException("Unknown serial name $serialName for generic parameter of AbstractFilter")
 		}
 
