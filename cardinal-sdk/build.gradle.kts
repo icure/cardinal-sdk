c5ec078f878d409bdbe9771788ace1e0b4b55f9b
Luca Tremamunno <tremaluca96@gmail.com>
2026-04-28 14:33:53 +0200

    Bump version

--- cardinal-sdk/build.gradle.kts
+++ cardinal-sdk/build.gradle.kts
@@ -18,7 +18,7 @@ val mavenReleasesRepository: String by project
 
 group = "com.icure"
 
-val version = "2.4.1"
+val version = "2.4.2"
 project.version = version
 
 val generateSdkVersion by tasks.registering {
