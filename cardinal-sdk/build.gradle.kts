3f7ba5744aa89c3b21d8417f788f9417457d0630
Vincenzo Pierro <vcp@icure.com>
2026-05-20 15:46:57 +0200

    Updated version to 2.5.0

--- cardinal-sdk/build.gradle.kts
+++ cardinal-sdk/build.gradle.kts
@@ -18,7 +18,7 @@ val mavenReleasesRepository: String by project
 
 group = "com.icure"
 
-val version = "2.4.5"
+val version = "2.5.0"
 project.version = version
 
 val generateSdkVersion by tasks.registering {
