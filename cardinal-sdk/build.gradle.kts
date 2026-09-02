ed4be29cb713a53117733704150de4b8d70ad68d
Clément Vandendaelen <cv@icure.com>
2026-09-02 11:36:34 +0200

    chore: Bump version (#688)

--- cardinal-sdk/build.gradle.kts
+++ cardinal-sdk/build.gradle.kts
@@ -18,7 +18,7 @@ val mavenReleasesRepository: String by project
 
 group = "com.icure"
 
-val version = "2.13.2"
+val version = "2.13.3"
 project.version = version
 
 val generateSdkVersion by tasks.registering {
